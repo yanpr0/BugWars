8a3ced7e823ed94a2d2fa9509fcc50b8e7fc9c48
Neo7k <neo7k@ya.ru>
2022-11-06 15:15:02 +0100

    Updated ChunkBordersCrossedOnUpdate test to handle reversed iteration.

--- BugWarsTest/Source/Code/TestBugsTargetSearch.cpp
+++ BugWarsTest/Source/Code/TestBugsTargetSearch.cpp
@@ -32,13 +32,23 @@ TEST(ChunkBordersCrossedOnUpdate)
 	bug_1st_target->position = Point(max_corner - 500.0f, min_corner); // top right corner, the closest target initially
 	game.AddObject(bug_1st_target);
 
+	// A target for reverse iteration update
+	auto bug_1st_target_r = new Bug;
+	bug_1st_target_r->position = Point(max_corner - 600.0f, min_corner); // top right corner, the closest target initially
+
 	auto bug_2nd_target = new Bug;
 	bug_2nd_target->position = Point(min_corner, max_corner - 100.0f); // bottom left corner, a bit further from the hunter bug
 	game.AddObject(bug_2nd_target);
 
+	auto bug_2nd_target_r = new Bug;
+	bug_2nd_target_r->position = Point(min_corner, max_corner - 200.0f); // bottom left corner, a bit further from the hunter bug
+
 	auto bug_hunter = new Bug; // very hungry
 	bug_hunter->position = Point(min_corner, min_corner);
 	game.AddObject(bug_hunter);
+
+	game.AddObject(bug_1st_target_r);
+	game.AddObject(bug_2nd_target_r);
 
 	game.OnBugsSpawned();
 	
@@ -51,7 +61,8 @@ TEST(ChunkBordersCrossedOnUpdate)
 
 	int frame_counter = 0;
 
-	game.onBugUpdate_End = [bug_1st_target, bug_2nd_target, &frame_counter](auto bug)
+	game.onBugUpdate_End = [bug_1st_target, bug_2nd_target, 
+		bug_1st_target_r, bug_2nd_target_r, &frame_counter](auto bug)
 	{
 		// On the second frame target bugs do huge jumps across the map
 		if (frame_counter != 1)
@@ -60,8 +71,14 @@ TEST(ChunkBordersCrossedOnUpdate)
 		if (bug == bug_1st_target)
 			bug_1st_target->position = bug_1st_target->position + Point(0.0f, 4000.0f);
 
+		if (bug == bug_1st_target_r)
+			bug_1st_target_r->position = bug_1st_target_r->position + Point(0.0f, 4000.0f);
+
 		if (bug == bug_2nd_target)
 			bug_2nd_target->position = bug_2nd_target->position - Point(0.0f, 4000.0f);
+
+		if (bug == bug_2nd_target_r)
+			bug_2nd_target_r->position = bug_2nd_target_r->position - Point(0.0f, 4000.0f);
 	};
 
 	game.Update(1.0f); // 1st frame
@@ -70,5 +87,5 @@ TEST(ChunkBordersCrossedOnUpdate)
 	// 2nd jumps closer to it
 	game.Update(1.0f); // 2nd frame
 
-	CHECK(target == bug_2nd_target);
+	CHECK(target == bug_2nd_target || target == bug_2nd_target_r);
 }
